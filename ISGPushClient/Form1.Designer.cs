686fcb0a1ca28359b29a83b60d7026edbd092e1c
ben <ben@BEN-PC>
2020-09-27 20:54:32 -0400

    1st commit

---
+++ ISGPushClient/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace ISGPushClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cboEnvironment = new System.Windows.Forms.ComboBox();
+            this.lblEnvironment = new System.Windows.Forms.Label();
+            this.cboTopic = new System.Windows.Forms.ComboBox();
+            this.lblTopic = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.txtInputFromFile = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.lstMessages = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnPush = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnClearForm = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cboEnvironment
+            // 
+            this.cboEnvironment.FormattingEnabled = true;
+            this.cboEnvironment.Location = new System.Drawing.Point(93, 12);
+            this.cboEnvironment.Name = "cboEnvironment";
+            this.cboEnvironment.Size = new System.Drawing.Size(350, 21);
+            this.cboEnvironment.TabIndex = 0;
+            this.cboEnvironment.SelectedIndexChanged += new System.EventHandler(this.cboEnvironment_SelectedIndexChanged);
+            // 
+            // lblEnvironment
+            // 
+            this.lblEnvironment.AutoSize = true;
+            this.lblEnvironment.Location = new System.Drawing.Point(12, 15);
+            this.lblEnvironment.Name = "lblEnvironment";
+            this.lblEnvironment.Size = new System.Drawing.Size(69, 13);
+            this.lblEnvironment.TabIndex = 1;
+            this.lblEnvironment.Text = "Environment:";
+            // 
+            // cboTopic
+            // 
+            this.cboTopic.FormattingEnabled = true;
+            this.cboTopic.Location = new System.Drawing.Point(93, 40);
+            this.cboTopic.Name = "cboTopic";
+            this.cboTopic.Size = new System.Drawing.Size(350, 21);
+            this.cboTopic.TabIndex = 2;
+            this.cboTopic.SelectedIndexChanged += new System.EventHandler(this.cboTopic_SelectedIndexChanged);
+            // 
+            // lblTopic
+            // 
+            this.lblTopic.AutoSize = true;
+            this.lblTopic.Location = new System.Drawing.Point(12, 40);
+            this.lblTopic.Name = "lblTopic";
+            this.lblTopic.Size = new System.Drawing.Size(37, 13);
+            this.lblTopic.TabIndex = 3;
+            this.lblTopic.Text = "Topic:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // txtInputFromFile
+            // 
+            this.txtInputFromFile.Location = new System.Drawing.Point(93, 220);
+            this.txtInputFromFile.Name = "txtInputFromFile";
+            this.txtInputFromFile.Size = new System.Drawing.Size(253, 20);
+            this.txtInputFromFile.TabIndex = 4;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(12, 68);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(34, 13);
+            this.lblInput.TabIndex = 5;
+            this.lblInput.Text = "Input:";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(368, 220);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 6;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // lstMessages
+            // 
+            this.lstMessages.FormattingEnabled = true;
+            this.lstMessages.Location = new System.Drawing.Point(12, 19);
+            this.lstMessages.Name = "lstMessages";
+            this.lstMessages.Size = new System.Drawing.Size(410, 212);
+            this.lstMessages.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClear);
+            this.groupBox1.Controls.Add(this.lstMessages);
+            this.groupBox1.Location = new System.Drawing.Point(12, 282);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(428, 275);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Application Messages";
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 238);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 8;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnPush
+            // 
+            this.btnPush.Location = new System.Drawing.Point(12, 253);
+            this.btnPush.Name = "btnPush";
+            this.btnPush.Size = new System.Drawing.Size(75, 23);
+            this.btnPush.TabIndex = 9;
+            this.btnPush.Text = "Push Messages";
+            this.btnPush.UseVisualStyleBackColor = true;
+            this.btnPush.Click += new System.EventHandler(this.btnPush_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(93, 67);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(350, 147);
+            this.txtInput.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 223);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Input from File:";
+            // 
+            // btnClearForm
+            // 
+            this.btnClearForm.Location = new System.Drawing.Point(93, 253);
+            this.btnClearForm.Name = "btnClearForm";
+            this.btnClearForm.Size = new System.Drawing.Size(75, 23);
+            this.btnClearForm.TabIndex = 12;
+            this.btnClearForm.Text = "Clear Form";
+            this.btnClearForm.UseVisualStyleBackColor = true;
+            this.btnClearForm.Click += new System.EventHandler(this.btnClearForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 595);
+            this.Controls.Add(this.btnClearForm);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnPush);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.txtInputFromFile);
+            this.Controls.Add(this.lblTopic);
+            this.Controls.Add(this.cboTopic);
+            this.Controls.Add(this.lblEnvironment);
+            this.Controls.Add(this.cboEnvironment);
+            this.Name = "Form1";
+            this.Text = "ISGPushClient";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cboEnvironment;
+        private System.Windows.Forms.Label lblEnvironment;
+        private System.Windows.Forms.ComboBox cboTopic;
+        private System.Windows.Forms.Label lblTopic;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox txtInputFromFile;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.ListBox lstMessages;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnPush;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClearForm;
+    }
+}
+
---
+++ ISGPushClient/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace ISGPushClient
+{
+    public partial class Form1 : Form
+    {
+        public static JObject appConfig = null;
+        string myConfig=null;
+       
+        public Form1()
+        {
+            InitializeComponent();
+            myConfig = File.ReadAllText("oedotnet.json");
+            appConfig = JObject.Parse(myConfig);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lstMessages.Items.Add("form Loading");
+            appConfig = JObject.Parse(myConfig);
+            var regions =
+                from p in appConfig["Application"]["Regions"]
+                select ((Newtonsoft.Json.Linq.JProperty)p).Name;
+            foreach (var item in regions)
+            {
+                cboEnvironment.Items.Add(item);
+                //Console.WriteLine(item);
+            }
+            lstMessages.Items.Add("regions loaded");
+        }
+
+        private void cboEnvironment_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (((ComboBox)sender).SelectedItem != null)
+            {
+                lstMessages.Items.Add("region: " + ((ComboBox)sender).SelectedItem + " selected");
+                Console.WriteLine(((ComboBox)sender).SelectedItem);
+                cboTopic.SelectedItem = null;
+                cboTopic.Items.Clear();
+                var topics =
+                    from p in appConfig["Application"]["Regions"][((ComboBox)sender).SelectedItem]["topics"]
+                    select (string)p["topic"];
+                foreach (var item in topics)
+                {
+                    cboTopic.Items.Add(item);
+                }
+                lstMessages.Items.Add("topics loaded");
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                Title = "Browse Text Files",
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txtInputFromFile.Text = openFileDialog1.FileName;
+            }
+            lstMessages.Items.Add("file: " + openFileDialog1.FileName + " selected");
+        }
+
+        private void cboTopic_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lstMessages.Items.Add("topic: " + ((ComboBox)sender).SelectedItem + " selected");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstMessages.Items.Clear();
+        }
+
+        private void btnPush_Click(object sender, EventArgs e)
+        {
+            if (txtInput.Text!="" && txtInputFromFile.Text!="") MessageBox.Show("You can select either a message or input file.");
+        }
+
+        private void btnClearForm_Click(object sender, EventArgs e)
+        {
+            cboEnvironment.SelectedItem = null;
+            cboTopic.Items.Clear();
+            cboTopic.SelectedItem = null;
+            cboTopic.Text = "";
+            txtInput.Text = "";
+            txtInputFromFile.Text = "";
+            lstMessages.Items.Clear();
+        }
+    }
+}
